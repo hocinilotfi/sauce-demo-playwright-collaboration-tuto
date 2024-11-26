a44c2a13af2f34035776342afd704e4060730660
BlondelMbou <blondel.mbou@gmail.com>
2024-11-26 12:52:02 +0100

    modification du fichier loginPage et logintest

--- tests/loginTest.spec.ts
+++ tests/loginTest.spec.ts
@@ -1,7 +1,7 @@
-import { test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/loginPage';
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../pages/loginPage";
 
-test.describe('Login Tests', () => {
+test.describe("Login Tests", () => {
   let loginPage: LoginPage;
 
   test.beforeEach(async ({ page }) => {
@@ -9,19 +9,25 @@ test.describe('Login Tests', () => {
     await loginPage.navigate();
   });
 
-  test('correct username && correct password', async () => {
-    await loginPage.login('standard_user', 'secret_sauce');
- });
+  test("correct username && correct password", async ({page}) => {
+    await loginPage.login("standard_user", "secret_sauce");
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 
-  test('correct username && incorrect password', async () => {
-    await loginPage.login('standard_user', 'wrong_password');
-    const errorMessage = await loginPage.getErrorMessage();
-    expect(errorMessage).toContain('Epic sadface: Username and password do not match any user in this service');
   });
 
-  test('incorrect username && correct password', async () => {
-    await loginPage.login('invalid_user', 'secret_sauce');
-    const errorMessage = await loginPage.getErrorMessage();
-    expect(errorMessage).toContain('Epic sadface: Username and password do not match any user in this service');
+  test("correct username && incorrect password", async () => {
+    await loginPage.login("standard_user", "wrong_password");
+    const errorMessage = await loginPage.elements.errorMessage().innerText();
+    expect(errorMessage).toContain(
+      "Epic sadface: Username and password do not match any user in this service"
+    );
+  });
+
+  test("incorrect username && correct password", async () => {
+    await loginPage.login("invalid_user", "secret_sauce");
+    const errorMessage = await loginPage.elements.errorMessage().innerText();
+    expect(errorMessage).toContain(
+      "Epic sadface: Username and password do not match any user in this service"
+    );
   });
 });
