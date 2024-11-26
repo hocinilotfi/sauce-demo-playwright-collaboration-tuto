a41500411401c9ae1950f7f50a3a92bbd4ca2367
BlondelMbou <blondel.mbou@gmail.com>
2024-11-26 15:52:40 +0100

    Modification de l'implementation de removetocardpage et removetocardtest

--- pages/removeToCardPage.ts
+++ pages/removeToCardPage.ts
@@ -1,23 +1,17 @@
 import { Page, Locator } from "@playwright/test";
 export class RemoveToCard {
   readonly page: Page;
-  readonly removeButton: Locator;
   elements = {
     removeButton: () => this.page.locator("button", { hasText: "Remove" }),
-    addToCartButton: () => this.page.locator('button[data-test="add-to-cart-sauce-labs-backpack"]'),
   };
 
   constructor(page: Page) {
     this.page = page;
   }
-  async addToCart() {
-    await this.elements.addToCartButton().click();
-  }
 
   async removeItem() {
     await this.elements.removeButton().click();
   }
-
 
   async isItemRemoved() {
     return !(await this.elements.removeButton().isVisible());
--- tests/removeToCardTest.spec.ts
+++ tests/removeToCardTest.spec.ts
@@ -1,22 +1,24 @@
 import { test, expect } from "@playwright/test";
 import { RemoveToCard } from "../pages/removeToCardPage";
 import { LoginPage } from "../pages/loginPage";
-import {AddToCartPage } from "../pages/addTocartPage";
+import { AddToCartPage } from "../pages/addTocartPage";
 
 test.describe("", () => {
   let loginPage: LoginPage;
+  let addToCartPage: AddToCartPage;
+  let removeToCard: RemoveToCard;
 
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page);
+    addToCartPage = new AddToCartPage(page);
+    removeToCard = new RemoveToCard(page);
     await loginPage.navigate();
   });
 
   test("Remove item from cart", async ({ page }) => {
     await loginPage.login("standard_user", "secret_sauce");
     await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
-    const addToCartPage = new AddToCartPage(page);
     await addToCartPage.clickAddToCart();
-    const removeToCard = new RemoveToCard(page);
     await removeToCard.removeItem();
     expect(await removeToCard.isItemRemoved()).toBeTruthy();
   });
